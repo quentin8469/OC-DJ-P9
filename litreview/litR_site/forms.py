e9c24bc7e55f526ea798590d18203653fffa4e27
Quentin Laparra <quentin.laparra@gmail.com>
2021-07-12 14:53:56 +0200

    creation forms inscription user

--- litreview/litR_site/forms.py
+++ litreview/litR_site/forms.py
@@ -3,6 +3,14 @@ from django import forms
 
 class NewUserForm(forms.Form):
     user_name = forms.CharField(max_length=8)
-    password = forms.CharField(max_length=6)
-    confirm_password = forms.CharField(max_length=6)
+    password = forms.CharField(max_length=6, widget=forms.PasswordInput())
+    confirm_password = forms.CharField(max_length=6, widget=forms.PasswordInput())
+
+    def clean_confirm_password(self):
+        confirm_password = self.cleaned_data.get("confirm_password")
+        password = self.cleaned_data.get("password")
+        if confirm_password != password:
+            raise forms.ValidationError("mot de passe different")
+        return confirm_password
+
 
--- litreview/litR_site/views.py
+++ litreview/litR_site/views.py
@@ -3,42 +3,87 @@ from .forms import NewUserForm
 
 
 def index(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "index.html")
 
 
 def signup(request):
-    form = NewUserForm()
+    """
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data
+            return render(request, "index.html")
+    else:
+        form = NewUserForm()
     return render(request, "signup.html", {"form": form})
 
 
 def flux(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "flux.html")
 
 
 def follow_users(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "follow_users.html")
 
 
 def create_ticket(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "create_ticket.html")
 
 
 def create_critics(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "create_critics.html")
 
 
 def answer_to_critic(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "answer_to_critic.html")
 
 
 def show_own_critics(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "show_own_critics.html")
 
 
 def edit_own_critics(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "show_own_critics.html")
 
 
 def edit_own_ticket(request):
+    """
+    :param request:
+    :return:
+    """
     return render(request, "edit_own_ticket.html")
-
