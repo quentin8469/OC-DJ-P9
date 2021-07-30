7398f6b1ed36d104a074f3d93e508d142d0dea3a
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-07-30 10:37:08 +0200

    add widgets in forms

--- litreview/litR_site/forms.py
+++ litreview/litR_site/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import RadioSelect, Textarea, TextInput
 
 from .models import Ticket, Review, UserFollows
 
@@ -22,12 +23,28 @@ class NewTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'image']
+        labels = {
+            "title": "Titre",
+            "description": "Description",
+            "image": "Image"
+        }
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['headline', 'rating', 'body']
+        labels = {
+            "rating": "Note",
+            "headline": "Titre",
+            "body": "Commentaire",
+        }
+        widgets = {
+            "rating": RadioSelect(
+                choices=[(0, "- 0"), (1, "- 1"), (2, "- 2"), (3, "- 3"), (4, "- 4"), (5, "- 5")]),
+            "body": Textarea(),
+            "headline": TextInput()
+        }
 
 
 class UserFollowForm(forms.ModelForm):
